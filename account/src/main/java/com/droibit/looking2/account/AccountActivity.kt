37613274a7ffa0471c57421088baf0002c05c21c
Shinya Kumagai <roomful.rooms@gmail.com>
2019-09-03 16:34:26 +0900

    Add account module

---
+++ account/src/main/java/com/droibit/looking2/account/AccountActivity.kt
@@ -0,0 +1,12 @@
+package com.droibit.looking2.account
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class AccountActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
