0b72efc5dca7cff61ede0ae13c6092ee0f7b5beb
Shinya Kumagai <roomful.rooms@gmail.com>
2019-09-11 22:26:17 +0900

    Update #authenticateTwitter

--- core/src/main/java/com/droibit/looking2/core/data/repository/account/AccountRepositoryImpl.kt
+++ core/src/main/java/com/droibit/looking2/core/data/repository/account/AccountRepositoryImpl.kt
@@ -2,17 +2,36 @@ package com.droibit.looking2.core.data.repository.account
 
 import com.droibit.looking2.core.data.CoroutinesDispatcherProvider
 import com.droibit.looking2.core.data.repository.account.service.TwitterAccountService
+import com.droibit.looking2.core.model.account.AuthenticationError
 import com.droibit.looking2.core.model.account.AuthenticationResult
+import com.droibit.looking2.core.model.account.AuthenticationResult.WillAuthenticateOnPhone
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import com.droibit.looking2.core.model.account.AuthenticationResult.Failure as AuthenticationFailure
+import com.droibit.looking2.core.model.account.AuthenticationResult.Success as AuthenticationSuccess
 
-class AccountRepositoryImpl @Inject constructor(
+internal class AccountRepositoryImpl @Inject constructor(
     private val twitterService: TwitterAccountService,
     private val dispatcherProvider: CoroutinesDispatcherProvider
 ) : AccountRepository {
 
     override suspend fun authenticateTwitter(): Flow<AuthenticationResult> = flow {
+        try {
+            val requestToken = twitterService.requestTempToken()
+
+            val responseUrl = withContext(dispatcherProvider.main) {
+                emit(WillAuthenticateOnPhone)
+                twitterService.sendAuthorizationRequest(requestToken)
+            }
+
+            val session = twitterService.createNewSession(requestToken, responseUrl)
+            // TODO: Store new session
+            emit(AuthenticationSuccess)
+        } catch (e: AuthenticationError) {
+            emit(AuthenticationFailure(error = e))
+        }
 
     }
 }
