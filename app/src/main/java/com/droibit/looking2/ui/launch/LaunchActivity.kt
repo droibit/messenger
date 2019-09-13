394fc11e7a9557d6c93ecc752cf1fbb9d21971cb
Shinya Kumagai <roomful.rooms@gmail.com>
2019-09-13 13:59:44 +0900

    Implementing login screen navigation from LaunchActivity

--- app/src/main/java/com/droibit/looking2/ui/launch/LaunchActivity.kt
+++ app/src/main/java/com/droibit/looking2/ui/launch/LaunchActivity.kt
@@ -1,17 +1,31 @@
 package com.droibit.looking2.ui.launch
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.observe
 import com.droibit.looking2.R
+import com.droibit.looking2.coreComponent
 import com.droibit.looking2.ui.Activities
+import javax.inject.Inject
 
 class LaunchActivity : FragmentActivity() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModel: LaunchViewModel by viewModels { viewModelFactory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        inject()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Activities.Account.createIntent(loginTwitter = false)
-        startActivity(intent)
+        viewModel.launchDestination.observe(this) {
+            val intent = Activities.Account.createIntent(loginTwitter = false)
+            startActivity(intent)
+        }
     }
 }
