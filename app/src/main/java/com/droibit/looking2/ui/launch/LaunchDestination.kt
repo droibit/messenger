1b01b21afa77c46a28622d7513df7907c64bf4cb
Shinya Kumagai <roomful.rooms@gmail.com>
2019-09-13 13:58:15 +0900

    Implement LaunchViewModel

---
+++ app/src/main/java/com/droibit/looking2/ui/launch/LaunchDestination.kt
@@ -0,0 +1,6 @@
+package com.droibit.looking2.ui.launch
+
+internal enum class LaunchDestination {
+    LOGIN_TWITTER,
+    HOME
+}
---
+++ app/src/main/java/com/droibit/looking2/ui/launch/LaunchViewModel.kt
@@ -0,0 +1,22 @@
+package com.droibit.looking2.ui.launch
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.droibit.looking2.core.data.repository.account.AccountRepository
+import javax.inject.Inject
+
+
+
+internal class LaunchViewModel @Inject constructor(
+    private val accountRepository: AccountRepository
+) : ViewModel() {
+
+    val launchDestination: LiveData<LaunchDestination> = liveData {
+        if (accountRepository.activeAccount() == null) {
+            emit(LaunchDestination.LOGIN_TWITTER)
+        } else {
+            emit(LaunchDestination.HOME)
+        }
+    }
+}
