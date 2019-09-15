f204a726efbeb781a66f1c9d92dde03edb6d25ce
Shinya Kumagai <roomful.rooms@gmail.com>
2019-09-16 02:31:11 +0900

    Implement to show SignInConfirmationDialogFragment

--- account/src/main/java/com/droibit/looking2/account/ui/signin/twitter/TwitterSignInFragment.kt
+++ account/src/main/java/com/droibit/looking2/account/ui/signin/twitter/TwitterSignInFragment.kt
@@ -6,20 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.wear.widget.SwipeDismissFrameLayout
 import com.droibit.looking2.account.R
 import com.droibit.looking2.account.databinding.FragmentTwitterSigninBinding
+import com.droibit.looking2.core.ui.dialog.DialogViewModel
+import com.droibit.looking2.core.util.ext.observeIfNotHandled
+import com.github.droibit.chopstick.resource.bindString
 import dagger.android.support.AndroidSupportInjection
 import timber.log.Timber
+import javax.inject.Inject
 import kotlin.LazyThreadSafetyMode.NONE
 
 class TwitterSignInFragment : Fragment() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
     private lateinit var binding: FragmentTwitterSigninBinding
 
+    private val dialogViewModel: DialogViewModel by activityViewModels { viewModelFactory }
+
+    private val confirmationMessage: String by bindString(R.string.account_sign_in_message_phone_preparation)
+
     private val swipeDismissCallback: SwipeDismissFrameLayout.Callback by lazy(NONE) {
-        object: SwipeDismissFrameLayout.Callback() {
+        object : SwipeDismissFrameLayout.Callback() {
             override fun onDismissed(layout: SwipeDismissFrameLayout) {
                 // Prevent flicker on screen.
                 layout.visibility = View.INVISIBLE
@@ -38,7 +51,9 @@ class TwitterSignInFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentTwitterSigninBinding.inflate(inflater, container, false)
+        binding = FragmentTwitterSigninBinding.inflate(inflater, container, false).also {
+            it.fragment = this
+        }
 
         val backStackEntryCount = requireFragmentManager().backStackEntryCount
         return if (backStackEntryCount == 0) binding.root else {
@@ -50,8 +65,25 @@ class TwitterSignInFragment : Fragment() {
         }
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        dialogViewModel.event.observeIfNotHandled(viewLifecycleOwner) { event ->
+            when (event.id.value) {
+                R.id.signInConfirmationDialogFragment -> {
+                    Timber.d("isok=${event.isOk}")
+                }
+            }
+        }
+    }
+
     override fun onDestroyView() {
         (view as? SwipeDismissFrameLayout)?.removeCallback(swipeDismissCallback)
         super.onDestroyView()
+    }
+
+    fun showConfirmDialog() {
+        val directions = TwitterSignInFragmentDirections.confirmTwitterSignIn(confirmationMessage)
+        findNavController().navigate(directions)
     }
 }
--- account/src/main/java/com/droibit/looking2/account/ui/signin/twitter/TwitterSignInModule.kt
+++ account/src/main/java/com/droibit/looking2/account/ui/signin/twitter/TwitterSignInModule.kt
@@ -1,7 +1,26 @@
 package com.droibit.looking2.account.ui.signin.twitter
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.droibit.looking2.core.di.key.ViewModelKey
+import com.droibit.looking2.core.ui.dialog.DialogViewModel
+import com.droibit.looking2.core.util.lifecycle.DaggerViewModelFactory
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
-@Module
+@Module(includes = [TwitterSignInModule.BindingModule::class])
 object TwitterSignInModule {
+
+    @Module
+    interface BindingModule {
+
+        @Binds
+        @IntoMap
+        @ViewModelKey(DialogViewModel::class)
+        fun bindDialogViewModel(viewModel: DialogViewModel): ViewModel
+
+        @Binds
+        fun bindViewModelFactory(factory: DaggerViewModelFactory): ViewModelProvider.Factory
+    }
 }
