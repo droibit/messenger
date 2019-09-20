3490028f27614c5b0bcb988f3f2c9170517eb612
Shinya Kumagai <roomful.rooms@gmail.com>
2019-09-20 18:39:16 +0900

    Update HomeActivity's layout

--- home/src/main/java/com/droibit/looking2/home/ui/HomeActivity.kt
+++ home/src/main/java/com/droibit/looking2/home/ui/HomeActivity.kt
@@ -1,13 +1,17 @@
 package com.droibit.looking2.home.ui
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import com.droibit.looking2.home.R
+import com.droibit.looking2.home.databinding.ActivityHomeBinding
 
 class HomeActivity : FragmentActivity() {
 
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
     }
 }
