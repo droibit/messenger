d3fbe0c310f4a159b44032d205424e6b77d1f26b
Shinya Kumagai <roomful.rooms@gmail.com>
2019-09-25 00:23:38 +0900

    Add GetTimelineCall

--- core/src/main/java/com/droibit/looking2/core/data/repository/timeline/TimelineRepository.kt
+++ core/src/main/java/com/droibit/looking2/core/data/repository/timeline/TimelineRepository.kt
@@ -1,8 +1,10 @@
 package com.droibit.looking2.core.data.repository.timeline
 
+import com.droibit.looking2.core.model.tweet.GetTimelineError
 import com.droibit.looking2.core.model.tweet.Tweet
 
 interface TimelineRepository {
 
+    @Throws(GetTimelineError::class)
     suspend fun getHomeTimeline(count: Int, sinceId: Long?): List<Tweet>
 }
--- timeline/src/main/java/com/droibit/looking2/timeline/ui/content/TimelineSource.kt
+++ timeline/src/main/java/com/droibit/looking2/timeline/ui/content/TimelineSource.kt
@@ -1,9 +1,36 @@
 package com.droibit.looking2.timeline.ui.content
 
+import com.droibit.looking2.core.data.repository.timeline.TimelineRepository
+import com.droibit.looking2.core.model.tweet.GetTimelineError
+import com.droibit.looking2.core.model.tweet.Tweet
 import java.io.Serializable
 
 sealed class TimelineSource : Serializable {
     object Home : TimelineSource()
     object Mentions : TimelineSource()
-    class MyList(val listId: Long) : TimelineSource()
+    class MyLists(val listId: Long) : TimelineSource()
+}
+
+interface GetTimelineCall {
+
+    @Throws(GetTimelineError::class)
+    suspend fun execute(sinceId: Long?): List<Tweet>
+
+    class Factory(
+        private val repository: TimelineRepository,
+        private val numOfTweetsGet: Int
+    ) {
+
+        fun create(source: TimelineSource): GetTimelineCall {
+            return when (source) {
+                is TimelineSource.Home -> object : GetTimelineCall {
+                    override suspend fun execute(sinceId: Long?): List<Tweet> {
+                        return repository.getHomeTimeline(count = numOfTweetsGet, sinceId = sinceId)
+                    }
+                }
+                is TimelineSource.Mentions -> TODO()
+                is TimelineSource.MyLists -> TODO()
+            }
+        }
+    }
 }
