9080e3c0bf1d3699cb194fc29eb3eae685968998
Shinya Kumagai <roomful.rooms@gmail.com>
2019-10-01 23:35:51 +0900

    Provide WorkManager

--- core/src/main/java/com/droibit/looking2/core/di/CoreModule.kt
+++ core/src/main/java/com/droibit/looking2/core/di/CoreModule.kt
@@ -2,24 +2,32 @@ package com.droibit.looking2.core.di
 
 import android.app.Application
 import android.content.Context
+import androidx.work.WorkManager
 import com.droibit.looking2.core.data.TwitterBootstrap
 import com.droibit.looking2.core.util.checker.PlayServicesChecker
 import com.google.android.gms.common.GoogleApiAvailability
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 object CoreModule {
 
-    @JvmStatic
     @Named("appContext")
     @Provides
+    @JvmStatic
     fun provideContext(application: Application): Context = application
 
-    @JvmStatic
     @Provides
+    @JvmStatic
     fun provideGoogleApiAvailability(): GoogleApiAvailability = GoogleApiAvailability.getInstance()
+
+    @Singleton
+    @Provides
+    @JvmStatic
+    fun provideWorkManager(@Named("appContext") context: Context): WorkManager
+        = WorkManager.getInstance(context)
 
     interface Provider {
 
@@ -34,5 +42,7 @@ object CoreModule {
         fun providePlayServicesChecker(): PlayServicesChecker
 
         fun provideTwitterBootstrap(): TwitterBootstrap
+
+        fun provideWorkManager(): WorkManager
     }
 }
