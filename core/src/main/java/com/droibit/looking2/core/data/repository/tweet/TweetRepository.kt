d39ec68d70efaa8fab43d0379b8c613b2f117feb
Shinya Kumagai <roomful.rooms@gmail.com>
2019-10-01 19:46:42 +0900

    WIP

--- core/src/main/java/com/droibit/looking2/core/data/repository/tweet/TweetRepository.kt
+++ core/src/main/java/com/droibit/looking2/core/data/repository/tweet/TweetRepository.kt
@@ -1,7 +1,9 @@
 package com.droibit.looking2.core.data.repository.tweet
 
+import com.droibit.looking2.core.model.tweet.RetweetError
+
 interface TweetRepository {
 
-    @Throws
+    @Throws(RetweetError::class)
     suspend fun retweet(tweetId: Long)
 }
--- timeline/src/main/java/com/droibit/looking2/timeline/ui/content/TweetAction.kt
+++ timeline/src/main/java/com/droibit/looking2/timeline/ui/content/TweetAction.kt
@@ -1,7 +1,13 @@
 package com.droibit.looking2.timeline.ui.content
 
+import android.content.Context
 import androidx.annotation.IdRes
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.droibit.looking2.core.data.repository.tweet.TweetRepository
+import com.droibit.looking2.core.model.tweet.RetweetError
 import com.droibit.looking2.core.model.tweet.Tweet
+import com.droibit.looking2.coreComponent
 import com.droibit.looking2.timeline.R
 
 data class TweetAction(val target: Tweet, val items: List<Item>) {
@@ -20,3 +26,33 @@ data class TweetAction(val target: Tweet, val items: List<Item>) {
     }
 }
 
+class RetweetActionWorker(
+    context: Context,
+    workerParams: WorkerParameters,
+    private val tweetRepository: TweetRepository
+) :
+    CoroutineWorker(context, workerParams) {
+
+    constructor(context: Context, workerParams: WorkerParameters) : this(
+        context,
+        workerParams,
+        context.coreComponent().provideTweetRepository()
+    )
+
+    override suspend fun doWork(): Result {
+        val tweetId = inputData.getLong(KEY_TWEET_ID, -1L)
+        check(tweetId != 1L)
+
+        return try {
+            tweetRepository.retweet(tweetId)
+            Result.success()
+        } catch (e: RetweetError) {
+            // TODO: retry
+            Result.failure()
+        }
+    }
+
+    companion object {
+        const val KEY_TWEET_ID = "KEY_TWEET_ID"
+    }
+}
