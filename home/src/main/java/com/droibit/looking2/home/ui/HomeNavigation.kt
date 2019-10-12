db0202391f277f937f28360eda2ef9abc5e37c59
Shinya Kumagai <roomful.rooms@gmail.com>
2019-10-12 19:37:10 +0900

    list -> lists

--- home/src/main/java/com/droibit/looking2/home/ui/HomeNavigation.kt
+++ home/src/main/java/com/droibit/looking2/home/ui/HomeNavigation.kt
@@ -3,11 +3,11 @@ package com.droibit.looking2.home.ui
 import androidx.annotation.IdRes
 import com.droibit.looking2.home.R
 
-enum class Navigation(@IdRes private val idRes: Int) {
+enum class HomeNavigation(@IdRes private val idRes: Int) {
     TWEET(R.id.action_tweet),
     TIMELINE(R.id.action_timeline),
     MENTIONS(R.id.action_mentions),
-    LIST(R.id.action_list),
+    LISTS(R.id.action_lists),
     ACCOUNTS(R.id.action_accounts),
     SETTINGS(R.id.action_settings);
 
