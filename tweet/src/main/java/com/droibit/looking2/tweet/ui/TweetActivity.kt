356b654c630c444fba961f482f62ebf8d8376a53
Shinya Kumagai <roomful.rooms@gmail.com>
2019-10-27 17:32:02 +0900

    Add tweet module

---
+++ tweet/src/main/java/com/droibit/looking2/tweet/ui/TweetActivity.kt
@@ -0,0 +1,12 @@
+package com.droibit.looking2.tweet.ui
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.droibit.looking2.tweet.R
+
+class TweetActivity : FragmentActivity(R.layout.activity_tweet) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
