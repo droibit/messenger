382f7df458cb182cc7b5c366f3bb9a2a2585e616
Shinya Kumagai <roomful.rooms@gmail.com>
2019-11-04 19:16:40 +0900

    Navigate to KeybaordTweetFragment

--- tweet/src/main/java/com/droibit/looking2/tweet/ui/chooser/TweetChooserFragment.kt
+++ tweet/src/main/java/com/droibit/looking2/tweet/ui/chooser/TweetChooserFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.droibit.looking2.tweet.databinding.FragmentTweetChooserBinding
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
@@ -29,6 +30,17 @@ class TweetChooserFragment : DaggerFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.fragment = this
         binding.screenTitle = title
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun onKeyboardTweetButtonClick(v: View) {
+        val direction = TweetChooserFragmentDirections.showKeyboardTweet()
+        findNavController().navigate(direction)
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun onVoiceTweetButtonClick(v: View) {
     }
 }
