f592a39f97aaf60991c883d608473b98c772c62f
Shinya Kumagai <roomful.rooms@gmail.com>
2019-12-31 21:52:18 +0900

    Update BindingAdapters

--- core/src/main/java/com/droibit/looking2/core/ui/BindingAdapters.kt
+++ core/src/main/java/com/droibit/looking2/core/ui/BindingAdapters.kt
@@ -1,9 +1,11 @@
 package com.droibit.looking2.core.ui
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.Px
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
 import androidx.databinding.BindingAdapter
 
@@ -22,16 +24,46 @@ fun bindGoneUnless(view: View, gone: Boolean, requestFocus: Boolean = false) {
 }
 
 @BindingAdapter("android:marginTop")
-fun bindPaddingTop(view: View, @Px marginPx: Int) {
-    view.updatePadding(top = marginPx)
+fun bindPaddingTop(view: View, @Px spacePx: Int) {
+    view.updatePadding(top = spacePx)
 }
 
 @BindingAdapter("android:paddingBottom")
-fun bindPaddingBottom(view: View, @Px marginPx: Int) {
-    view.updatePadding(bottom = marginPx)
+fun bindPaddingBottom(view: View, @Px spacePx: Int) {
+    view.updatePadding(bottom = spacePx)
+}
+
+@BindingAdapter("android:paddingStart")
+fun bindPaddingStart(view: View, @Px spacePx: Int) {
+    view.updatePadding(left = spacePx)
+}
+
+@BindingAdapter("android:paddingEnd")
+fun bindPaddingEnd(view: View, @Px spacePx: Int) {
+    view.updatePadding(right = spacePx)
+}
+
+
+@BindingAdapter("android:layout_marginTop")
+fun bindMarginTop(view: View, @Px spacePx: Int) {
+    view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        topMargin = spacePx
+    }
+}
+
+@BindingAdapter("android:layout_marginBottom")
+fun bindMarginBottom(view: View, @Px spacePx: Int) {
+    view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        bottomMargin = spacePx
+    }
 }
 
 @BindingAdapter("enabled")
 fun bindEnabled(view: View, enabled: Boolean) {
     view.isEnabled = enabled
+}
+
+@BindingAdapter("activated")
+fun bindActivated(view: View, activated: Boolean) {
+    view.isActivated = activated
 }
