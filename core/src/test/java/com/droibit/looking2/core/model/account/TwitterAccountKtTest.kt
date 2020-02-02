ada11d49eaec92ce0786a0d99146434a7be00dec
Shinya Kumagai <roomful.rooms@gmail.com>
2020-02-03 00:35:34 +0900

    Add TwitterAccountKtTest

---
+++ core/src/test/java/com/droibit/looking2/core/model/account/TwitterAccountKtTest.kt
@@ -0,0 +1,24 @@
+package com.droibit.looking2.core.model.account
+
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.twitter.sdk.android.core.TwitterSession
+import org.junit.Test
+
+class TwitterAccountKtTest {
+
+    @Test
+    fun toAccount_TwitterSession() {
+        val session = mock<TwitterSession> {
+            on { this.userId } doReturn 1L
+            on { this.userName } doReturn "test"
+        }
+        assertThat(session.toAccount(active = true)).isEqualTo(
+            TwitterAccount(1L, "test", active = true)
+        )
+        assertThat(session.toAccount(active = false)).isEqualTo(
+            TwitterAccount(1L, "test", active = false)
+        )
+    }
+}
