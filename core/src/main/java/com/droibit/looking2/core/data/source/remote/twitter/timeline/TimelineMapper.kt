2fadf9d9ff6ad04df1972c5d7ff1f310c035ae6f
Shinya Kumagai <roomful.rooms@gmail.com>
2020-02-10 02:22:29 +0900

    Refactor TimelineMapper

--- core/src/main/java/com/droibit/looking2/core/data/source/remote/twitter/timeline/TimelineMapper.kt
+++ core/src/main/java/com/droibit/looking2/core/data/source/remote/twitter/timeline/TimelineMapper.kt
@@ -7,22 +7,22 @@ import com.droibit.looking2.core.model.tweet.User
 import com.droibit.looking2.core.util.ext.unescapeHtml
 import com.twitter.sdk.android.core.TwitterException
 import com.twitter.sdk.android.core.models.TweetEntities
+import java.text.DateFormat
 import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.Locale
 import javax.inject.Inject
+import javax.inject.Named
 import com.droibit.looking2.core.model.tweet.Media.Photo as PhotoMedia
 import com.droibit.looking2.core.model.tweet.Media.Unsupported as UnsupportedMedia
 import com.twitter.sdk.android.core.models.Tweet as TweetResponse
 import com.twitter.sdk.android.core.models.User as UserResponse
 
-// TODO: Consider using ThreadLocal when parsing dates with multithreading.
-private val dateTimeRFC822 = SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH)
 private const val MEDIA_TYPE_PHOTO = "photo"
 private const val PROFILE_ICON_SIZE_NORMAL = "_normal"
 private const val PROFILE_ICON_SIZE_BIGGER = "_bigger"
 
-class TimelineMapper @Inject constructor() {
+class TimelineMapper @Inject constructor(
+    @Named("twitterApi") private val dateFormat: DateFormat
+) {
 
     @Throws(TwitterException::class)
     fun toTimeline(source: List<TweetResponse>): List<Tweet> {
@@ -38,76 +38,76 @@ class TimelineMapper @Inject constructor() {
             throw TwitterException("Parse Failure", e)
         }
     }
-}
 
-@Throws(ParseException::class)
-private fun TweetResponse.toTweet(): Tweet {
-    return Tweet(
-        id = id,
-        text = text.unescapeHtml(),
-        createdAt = parseTime(
-            createdAt
-        ),
-        urls = entities.toShorteningUrls(),
-        medium = extendedEntities.toMediaShorteningUrls(),
-        user = user.toUser(),
-        liked = favorited,
-        retweeted = retweeted,
-        retweetedTweet = null,
-        quotedTweet = quotedStatus?.toTweet()
-    )
-}
-
-@Throws(ParseException::class)
-private fun TweetResponse.toRetweetedTweet(): Tweet {
-    return Tweet(
-        id = id,
-        text = "",
-        createdAt = parseTime(
-            createdAt
-        ),
-        urls = emptyList(),
-        medium = emptyList(),
-        user = user.toUser(),
-        liked = favorited,
-        retweeted = retweeted,
-        retweetedTweet = retweetedStatus.toTweet(),
-        quotedTweet = quotedStatus?.toTweet()
-    )
-}
-
-private fun TweetEntities?.toShorteningUrls(): List<ShorteningUrl> {
-    val urls = this?.urls ?: return emptyList()
-    return urls.map {
-        ShorteningUrl(it.url, it.displayUrl, it.expandedUrl)
+    @Throws(ParseException::class)
+    private fun TweetResponse.toTweet(): Tweet {
+        return Tweet(
+            id = id,
+            text = text.unescapeHtml(),
+            createdAt = parseTime(
+                createdAt
+            ),
+            urls = entities.toShorteningUrls(),
+            medium = extendedEntities.toMediaShorteningUrls(),
+            user = user.toUser(),
+            liked = favorited,
+            retweeted = retweeted,
+            retweetedTweet = null,
+            quotedTweet = quotedStatus?.toTweet()
+        )
     }
-}
 
-private fun TweetEntities?.toMediaShorteningUrls(): List<Media> {
-    val media = this?.media ?: return emptyList()
-    return media.map {
-        when (it.type) {
-            MEDIA_TYPE_PHOTO -> PhotoMedia(
-                ShorteningUrl(it.url, it.displayUrl, "${it.mediaUrlHttps}:small")
-            )
-            // e.g. `video`, `animated_gif` ...
-            else -> UnsupportedMedia(
-                it.type,
-                ShorteningUrl(it.url, it.displayUrl, it.mediaUrlHttps)
-            )
+    @Throws(ParseException::class)
+    private fun TweetResponse.toRetweetedTweet(): Tweet {
+        return Tweet(
+            id = id,
+            text = "",
+            createdAt = parseTime(
+                createdAt
+            ),
+            urls = emptyList(),
+            medium = emptyList(),
+            user = user.toUser(),
+            liked = favorited,
+            retweeted = retweeted,
+            retweetedTweet = retweetedStatus.toTweet(),
+            quotedTweet = quotedStatus?.toTweet()
+        )
+    }
+
+    private fun TweetEntities?.toShorteningUrls(): List<ShorteningUrl> {
+        val urls = this?.urls ?: return emptyList()
+        return urls.map {
+            ShorteningUrl(it.url, it.displayUrl, it.expandedUrl)
         }
     }
-}
 
-private fun UserResponse.toUser() = User(
-    id, name, screenName,
-    profileUrl = profileImageUrlHttps.replaceFirst(
-        PROFILE_ICON_SIZE_NORMAL,
-        PROFILE_ICON_SIZE_BIGGER
+    private fun TweetEntities?.toMediaShorteningUrls(): List<Media> {
+        val media = this?.media ?: return emptyList()
+        return media.map {
+            when (it.type) {
+                MEDIA_TYPE_PHOTO -> PhotoMedia(
+                    ShorteningUrl(it.url, it.displayUrl, "${it.mediaUrlHttps}:small")
+                )
+                // e.g. `video`, `animated_gif` ...
+                else -> UnsupportedMedia(
+                    it.type,
+                    ShorteningUrl(it.url, it.displayUrl, it.mediaUrlHttps)
+                )
+            }
+        }
+    }
+
+    private fun UserResponse.toUser() = User(
+        id, name, screenName,
+        profileUrl = profileImageUrlHttps.replaceFirst(
+            PROFILE_ICON_SIZE_NORMAL,
+            PROFILE_ICON_SIZE_BIGGER
+        )
     )
-)
 
-@Throws(ParseException::class)
-private fun parseTime(time: String): Long {
-    return requireNotNull(dateTimeRFC822.parse(time)).time
+    @Throws(ParseException::class)
+    private fun parseTime(time: String): Long {
+        return requireNotNull(dateFormat.parse(time)).time
+    }
 }
