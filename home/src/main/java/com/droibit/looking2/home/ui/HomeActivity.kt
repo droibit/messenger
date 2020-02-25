08ed1cca491e387242ceda7ac7e6b2deb65cd060
Shinya Kumagai <roomful.rooms@gmail.com>
2020-02-25 22:31:17 +0900

    Extract from Activity as HomeFragment

--- home/src/main/java/com/droibit/looking2/home/ui/HomeActivity.kt
+++ home/src/main/java/com/droibit/looking2/home/ui/HomeActivity.kt
@@ -1,79 +1,13 @@
 package com.droibit.looking2.home.ui
 
-import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.observe
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.droibit.looking2.core.ui.widget.ActionItemListAdapter
-import com.droibit.looking2.core.ui.widget.ActionItemListAdapter.ActionItem
-import com.droibit.looking2.core.util.analytics.AnalyticsHelper
-import com.droibit.looking2.core.util.analytics.sendScreenView
-import com.droibit.looking2.home.databinding.ActivityHomeBinding
-import com.droibit.looking2.home.ui.HomeNavigation.ACCOUNTS
-import com.droibit.looking2.home.ui.HomeNavigation.LISTS
-import com.droibit.looking2.home.ui.HomeNavigation.MENTIONS
-import com.droibit.looking2.home.ui.HomeNavigation.SETTINGS
-import com.droibit.looking2.home.ui.HomeNavigation.TIMELINE
-import com.droibit.looking2.home.ui.HomeNavigation.TWEET
-import javax.inject.Inject
-import com.droibit.looking2.core.R as coreR
-import com.droibit.looking2.home.R as homeR
-import com.droibit.looking2.ui.Activities.Account as AccountActivity
-import com.droibit.looking2.ui.Activities.Settings as SettingsActivity
-import com.droibit.looking2.ui.Activities.Timeline as TimelineActivity
-import com.droibit.looking2.ui.Activities.Tweet as TweetActivity
+import com.droibit.looking2.home.R
 
-class HomeActivity : FragmentActivity() {
-
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
-
-    @Inject
-    lateinit var analytics: AnalyticsHelper
-
-    @Inject
-    lateinit var actionItemListAdapter: ActionItemListAdapter
-
-    private lateinit var binding: ActivityHomeBinding
-
-    private val viewModel: HomeViewModel by viewModels { viewModelFactory }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        inject()
-        super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this, homeR.layout.activity_home)
-        binding.navigationList.apply {
-            adapter = actionItemListAdapter
-        }
-
-        viewModel.activeAccountName.observe(this) {
-            actionItemListAdapter.title = getString(coreR.string.twitter_account_name_with_at, it)
-
-            binding.navigationList.apply {
-                (layoutManager as LinearLayoutManager).scrollToPositionWithOffset(0, 0)
-                requestFocus()
-            }
-        }
-    }
+class HomeActivity : FragmentActivity(R.layout.activity_home) {
 
     override fun onResume() {
         super.onResume()
-        analytics.sendScreenView(homeR.string.home_nav_label_home, this)
-    }
-
-    fun onActionItemClick(item: ActionItem) {
-        val intent = when (HomeNavigation(item.id)) {
-            TWEET -> TweetActivity.createIntent()
-            TIMELINE -> TimelineActivity.createHomeIntent()
-            MENTIONS -> TimelineActivity.createMentionsIntent()
-            LISTS -> TimelineActivity.createListsIntent()
-            ACCOUNTS -> AccountActivity.createIntent(needTwitterSignIn = false)
-            SETTINGS -> SettingsActivity.createIntent()
-        }
-        startActivity(intent)
+        // TODO:
+        // analytics.sendScreenView(homeR.string.home_nav_label_home, this)
     }
 }
---
+++ home/src/main/java/com/droibit/looking2/home/ui/HomeFragment.kt
@@ -0,0 +1,79 @@
+package com.droibit.looking2.home.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.droibit.looking2.core.R
+import com.droibit.looking2.core.ui.widget.ActionItemListAdapter
+import com.droibit.looking2.core.ui.widget.ActionItemListAdapter.ActionItem
+import com.droibit.looking2.home.databinding.FragmentHomeBinding
+import com.droibit.looking2.ui.Activities
+import javax.inject.Inject
+
+class HomeFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    @Inject
+    lateinit var actionItemListAdapter: ActionItemListAdapter
+
+    private val viewModel: HomeViewModel by viewModels { viewModelFactory }
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        inject()
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.navigationList.apply {
+            adapter = actionItemListAdapter
+        }
+
+        viewModel.activeAccountName.observe(viewLifecycleOwner) {
+            actionItemListAdapter.title = getString(R.string.twitter_account_name_with_at, it)
+
+            binding.navigationList.apply {
+                (layoutManager as LinearLayoutManager).scrollToPositionWithOffset(0, 0)
+                requestFocus()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    fun onActionItemClick(item: ActionItem) {
+        val intent = when (HomeNavigation(item.id)) {
+            HomeNavigation.TWEET -> Activities.Tweet.createIntent()
+            HomeNavigation.TIMELINE -> Activities.Timeline.createHomeIntent()
+            HomeNavigation.MENTIONS -> Activities.Timeline.createMentionsIntent()
+            HomeNavigation.LISTS -> Activities.Timeline.createListsIntent()
+            HomeNavigation.ACCOUNTS -> Activities.Account.createIntent(needTwitterSignIn = false)
+            HomeNavigation.SETTINGS -> Activities.Settings.createIntent()
+        }
+        startActivity(intent)
+    }
+}
