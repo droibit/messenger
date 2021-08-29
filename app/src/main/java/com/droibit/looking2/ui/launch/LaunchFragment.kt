a0ccc5ab27238f8dc66d73b0c2530b70a0161a66
Shinya Kumagai <roomful.rooms@gmail.com>
2021-08-30 02:43:34 +0900

    Delete unused dagger codes

--- app/src/main/java/com/droibit/looking2/ui/launch/LaunchFragment.kt
+++ app/src/main/java/com/droibit/looking2/ui/launch/LaunchFragment.kt
@@ -3,7 +3,6 @@ package com.droibit.looking2.ui.launch
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.droibit.looking2.R
 import com.droibit.looking2.core.ui.Activities.Account as AccountActivity
 import com.droibit.looking2.core.ui.Activities.Home as HomeActivity
@@ -15,15 +14,11 @@ import javax.inject.Inject
 class LaunchFragment : Fragment() {
 
     @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
-
-    @Inject
     lateinit var analytics: AnalyticsHelper
 
-    private val viewModel: LaunchViewModel by viewModels { viewModelFactory }
+    private val viewModel: LaunchViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        inject()
         super.onCreate(savedInstanceState)
 
         viewModel.launchDestination.observe(this) {
--- core/src/main/java/com/droibit/looking2/core/di/CoreModule.kt
+++ core/src/main/java/com/droibit/looking2/core/di/CoreModule.kt
@@ -1,33 +1,21 @@
 package com.droibit.looking2.core.di
 
-import android.app.Application
 import android.content.Context
-import androidx.lifecycle.ViewModelProvider
 import androidx.work.WorkManager
 import com.droibit.looking2.core.config.AccountConfiguration
-import com.droibit.looking2.core.config.AppVersion
-import com.droibit.looking2.core.data.TwitterBootstrap
 import com.droibit.looking2.core.util.analytics.AnalyticsHelper
 import com.droibit.looking2.core.util.analytics.FirebaseAnalyticsHelper
-import com.droibit.looking2.core.util.checker.PhoneDeviceTypeChecker
-import com.droibit.looking2.core.util.lifecycle.DaggerViewModelFactory
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Named
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
-@Module(includes = [CoreModule.BindingModule::class])
+@Module
 object CoreModule {
-
-    @ApplicationContext
-    @Provides
-    @Deprecated("Migrate to dagger hilt.")
-    fun provideContext(application: Application): Context = application
 
     @Provides
     fun provideWorkManager(@ApplicationContext context: Context): WorkManager =
@@ -47,32 +35,5 @@ object CoreModule {
 
         @Binds
         fun bindAnalyticsHelper(analytics: FirebaseAnalyticsHelper): AnalyticsHelper
-
-        @Binds
-        fun bindViewModelFactory(factory: DaggerViewModelFactory): ViewModelProvider.Factory
-    }
-
-    @Deprecated("Migrate to dagger hilt.")
-    interface Provider {
-
-        fun provideApplication(): Application
-
-        @ApplicationContext
-        fun provideApplicationContext(): Context
-
-        @Named("debuggable")
-        fun provideDebuggable(): Boolean
-
-        fun provideAppVersion(): AppVersion
-
-        fun provideTwitterBootstrap(): TwitterBootstrap
-
-        fun provideWorkManager(): WorkManager
-
-        fun provideAccountConfiguration(): AccountConfiguration
-
-        fun provideAnalytics(): AnalyticsHelper
-
-        fun providePhoneDeviceTypeChecker(): PhoneDeviceTypeChecker
     }
 }
